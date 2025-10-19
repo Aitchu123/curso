910070950d983db686741fa5554a46270eb77e27
Aitchu123 <loschimarcelo@gmail.com>
2025-10-19 11:23:06 -0300

    feat: Adiciona botão flutuante do WhatsApp com animações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
+import WhatsAppButton from './components/ui/WhatsAppButton'
 
 
 function App() {
   return (
     <BrowserRouter basename={__BASE_PATH__}>
       <AppRoutes />
+      <WhatsAppButton />
     </BrowserRouter>
   )
 }
