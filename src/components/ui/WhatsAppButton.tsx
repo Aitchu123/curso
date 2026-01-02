8d3b13ce28c68fcb563af6c2c28492bcb10e736e
Aitchu123 <loschimarcelo@gmail.com>
2026-01-02 09:08:57 -0300

    add number 12982576502 to whatsapp pool

--- src/components/ui/WhatsAppButton.tsx
+++ src/components/ui/WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WhatsAppButton: React.FC = () => {
   const handleWhatsAppClick = () => {
     const message = encodeURIComponent('Quero saber mais');
-    const pool = ['5512982640901', '5521972762708', '5512978133810', '5512997982655'];
+    const pool = ['5512982640901', '5521972762708', '5512978133810', '5512997982655', '5512982576502'];
     let idx = 0;
     try {
       const buf = new Uint32Array(1);
